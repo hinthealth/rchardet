1a0d096ab5b8a64d4534de9179194d1cc402e7e2
MaicolBen <maicol.bentancor@gmail.com>
2024-03-14 15:56:21 -0300

    Support windows encoding

--- lib/rchardet/sbcharsetprober.rb
+++ lib/rchardet/sbcharsetprober.rb
@@ -15,12 +15,12 @@
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
@@ -110,7 +110,7 @@ module CharDet
     def get_confidence
       r = 0.01
       if @totalSeqs > 0
-        r = (1.0 * @seqCounters[POSITIVE_CAT]) / @totalSeqs / @model['mTypicalPositiveRatio']
+        r = @seqCounters[POSITIVE_CAT].to_f / @totalSeqs / @model['mTypicalPositiveRatio']
         r = r * @freqChar / @totalChar
         if r >= 1.0
           r = 0.99
